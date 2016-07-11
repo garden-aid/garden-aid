71ec2aa52f707bcec91044f2215041b276f6a49c
John McKim <johncmckim@gmail.com>
2016-07-11 18:47:07 +1000

    Start looking at data types

--- web-bff/src/handler.js
+++ web-bff/src/handler.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var userType = new graphql.GraphQLObjectType({
+  name: 'Day',
+  fields: {
+    hour: { type: graphql.GraphQLString },
+    moisture: { type: graphql.GraphQLFloat },
+  }
+});
+
 // Your first function handler
 module.exports.hello = (event, context, cb) => cb(null,
   { message: 'Go Serverless v1.0! Your function executed successfully!', event }
--- web/README.md
+++ web/README.md
@@ -2,3 +2,4 @@
 
 ## Research
 * https://medium.com/apollo-stack/apollo-client-graphql-with-react-and-redux-49b35d0f2641#.sk17i87wv
+* 
