74a3e403f84aeb594325dd60b5e8259fd4d9495e
John McKim <johncmckim@gmail.com>
2016-07-26 17:48:54 +1000

    Start adding IOT services

--- iot-hub/src/handler.js
+++ iot-hub/src/handler.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// Your first function handler
-module.exports.hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
-);
-
-// You can add more handlers here, and reference them in serverless.yaml
+module.exports.processMoisture = (event, context, cb) => {
+  console.log(event);
+  cb(null, { message: 'success' });
+}
