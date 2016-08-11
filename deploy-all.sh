683fd666f7d2a43ca0a128f9bba38640b6b55f8c
John McKim <johncmckim@gmail.com>
2016-08-12 06:40:02 +1000

    Start adding ci build

--- deploy-all.sh
+++ deploy-all.sh
@@ -3,6 +3,26 @@ set -e
 
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bash "$WORKING_DIR/iot-hub/deploy.sh"
-bash "$WORKING_DIR/web-bff/deploy.sh"
-bash "$WORKING_DIR/web/deploy.sh"
+if [[ $TRAVIS_PULL_REQUEST == "true" ]]; then
+  echo "Not deploying on pull request"
+  exit 1;
+fi
+
+BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+
+if [[ $BRANCH == 'master' ]]; then
+  STAGE="prod"
+elif [[ $BRANCH == 'develop' ]]; then
+  STAGE="dev"
+fi
+
+if [ -z ${STAGE+x} ]; then
+  echo "Not deploying changes";
+  exit 0;
+fi
+
+echo "Deploying stage from branch $BRANCH to $STAGE"
+
+. "$WORKING_DIR/iot-hub/deploy.sh"
+. "$WORKING_DIR/web-bff/deploy.sh"
+. "$WORKING_DIR/web/deploy.sh"
